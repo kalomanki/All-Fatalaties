af597e3fc8978c178ad7ecd5c097b0a41442cfdb
kalomanki <107642071+kalomanki@users.noreply.github.com>
2022-11-21 19:21:09 +0300

    Add files via upload

---
+++ ladno.js
@@ -0,0 +1,11 @@
+let butt = document.querySelector('.chillyman');
+let body = document.querySelector('body');
+
+butt.onclick = function(){
+    body.style.backgroundImage = 'url(img/meshdunamitaetled.jpg)';
+}
+
+
+
+
+
